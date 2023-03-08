7d997a74594aba816b284438ca2d5f22ed543997
xin gu <418294249@qq.com>
2023-03-08 22:19:26 +0800

    Update testutils_test.go
    Signed-off-by: xin gu <418294249@qq.com>

--- internal/store/testutils_test.go
+++ internal/store/testutils_test.go
@@ -22,11 +22,11 @@ import (
 )
 
 func TestSortLabels(t *testing.T) {
-	in := `kube_pod_container_info{container_id="docker://cd456",image="k8s.gcr.io/hyperkube2",container="container2",image_id="docker://sha256:bbb",namespace="ns2",pod="pod2"} 1
-kube_pod_container_info{namespace="ns2",container="container3",container_id="docker://ef789",image="k8s.gcr.io/hyperkube3",image_id="docker://sha256:ccc",pod="pod2"} 1`
+	in := `kube_pod_container_info{container_id="docker://cd456",image="registry.k8s.io/hyperkube2",container="container2",image_id="docker://sha256:bbb",namespace="ns2",pod="pod2"} 1
+kube_pod_container_info{namespace="ns2",container="container3",container_id="docker://ef789",image="registry.k8s.io/hyperkube3",image_id="docker://sha256:ccc",pod="pod2"} 1`
 
-	want := `kube_pod_container_info{container="container2",container_id="docker://cd456",image="k8s.gcr.io/hyperkube2",image_id="docker://sha256:bbb",namespace="ns2",pod="pod2"} 1
-kube_pod_container_info{container="container3",container_id="docker://ef789",image="k8s.gcr.io/hyperkube3",image_id="docker://sha256:ccc",namespace="ns2",pod="pod2"} 1`
+	want := `kube_pod_container_info{container="container2",container_id="docker://cd456",image="registry.k8s.io/hyperkube2",image_id="docker://sha256:bbb",namespace="ns2",pod="pod2"} 1
+kube_pod_container_info{container="container3",container_id="docker://ef789",image="registry.k8s.io/hyperkube3",image_id="docker://sha256:ccc",namespace="ns2",pod="pod2"} 1`
 
 	out := sortLabels(in)
 
